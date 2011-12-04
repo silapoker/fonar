0e140b4661257b49d92772a0c8a19c86c4edc45f
Silaev D <silaev.developer@gmail.com>
2011-12-04 18:37:22 +0400

    'asasa'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//menu
+$(document).ready(function () {
+//найти текущую позицию активного элемента
+var dleft = $('.lavalamp li.active').offset().left - $('.lavalamp').offset().left;
+var dwidth = $('.lavalamp li.active').width() + "px";
+//присвоить эту позицию элеметку с подсветкой
+$('.floatr').css({
+"left": dleft+"px",
+"width": dwidth
+});
+$('.lavalamp li').hover(function(){
+var left = $(this).offset().left - ($(this).parents('.lavalamp').offset().left + 15);
+var width = $(this).width() + "px";
+var sictranslate = "translate("+left+"px, 0px)";
+$(this).parent('ul').next('div.floatr').css({
+"width": width,
+"-webkit-transform": sictranslate,
+"-moz-transform": sictranslate
+});
+},
+function(){
+var left = $(this).siblings('li.active').offset().left - ($(this).parents('.lavalamp').offset().left + 15);
+var width = $(this).siblings('li.active').width() + "px";
+var sictranslate = "translate("+left+"px, 0px)";
+$(this).parent('ul').next('div.floatr').css({
+"width": width,
+"-webkit-transform": sictranslate,
+"-moz-transform": sictranslate
+});
+}).click(function(){
+$(this).live.siblings('li').removeClass('active');
+$(this).live.addClass('active');
+return false;
+});
+});
+//konec
