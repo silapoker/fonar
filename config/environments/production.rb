acb9f2b94d62b42421f62efef59a11a8a72b4b08
Dmitry <silapoker@mail.ru>
2011-12-07 23:46:13 +0400

    dd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ New::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-config.assets.precompile = true
+config.assets.compile = true
+
 
   # Generate digests for assets URLs
   config.assets.digest = true
