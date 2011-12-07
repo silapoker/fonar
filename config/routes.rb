1b7b42f2bdfa5978c9f71f43daec755d21455ffd
Dmitry <silapoker@mail.ru>
2011-12-07 18:41:44 +0400

    ad

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ match 'manual' => 'pages#manual'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'zakazs#new'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
